f2cc2fba29b493a322fddbed2af492f9e952d309
NanoSoft-1C <nanosoft-1c@yandex.ru>
2025-10-22 10:19:06 +0300

    test change

--- error.vue
+++ error.vue
@@ -2,6 +2,6 @@
   <div class="errorPage">
     <h1 class="errorPage__h1 title-h1">Что тут?</h1>
     <video class="errorPage__video" autoplay="autoplay" muted src="@/assets/video/error.mp4"></video>
-    <h1 class="errorPage__h1 title-h1">А тут пусто</h1>
+    <h1 class="errorPage__h1 title-h1">А тут пусто, всё</h1>
   </div>
 </template>
