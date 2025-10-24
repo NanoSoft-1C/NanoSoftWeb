f8895c58db6de2c6b14448cabb107b56b54466ea
NanoSoft-1C <nanosoft-1c@yandex.ru>
2025-10-24 17:50:56 +0300

    del 404 page

--- error.vue
+++ error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="errorPage">
-    <h1 class="errorPage__h1 title-h1">Что тут?</h1>
-    <video class="errorPage__video" autoplay="autoplay" muted src="@/assets/video/error.mp4"></video>
-    <h1 class="errorPage__h1 title-h1">А тут пусто, всё</h1>
+    <h1 class="errorPage__h1 title-h1">Технические проблемы</h1>
+    <!-- <video class="errorPage__video" autoplay="autoplay" muted src="@/assets/video/error.mp4"></video>
+    <h1 class="errorPage__h1 title-h1">А тут пусто, всё</h1> -->
   </div>
 </template>
