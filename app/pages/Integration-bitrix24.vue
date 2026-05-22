68cac1ad1cba1862e5d7a57403b9b0e867f391d6
NanoSoft-1C <nanosoft-1c@yandex.ru>
2026-05-22 10:36:07 +0300

    Добавлена новая страница Битрикс24

--- app/pages/Integration-bitrix24.vue
+++ app/pages/Integration-bitrix24.vue
@@ -1,11 +1,4 @@
 <script setup>
-import BitrixHero from '~/widgets/bitrix24/Hero/ui/BitrixHero.vue'
-import BitrixAdvantages from '~/widgets/bitrix24/Advantages/ui/BitrixAdvantages.vue'
-import BitrixServices from '~/widgets/bitrix24/Services/ui/BitrixServices.vue'
-import BitrixProcess from '~/widgets/bitrix24/Process/ui/BitrixProcess.vue'
-import BitrixCTA from '~/widgets/bitrix24/CTA/ui/BitrixCTA.vue'
-
-
 useSeoMeta({
   title: 'Интеграция Битрикс24 — NanoSoft',
   description: 'Интеграция Битрикс24 и 1С. Автоматизация CRM и бизнес-процессов.',
@@ -15,15 +8,15 @@ useSeoMeta({
 <template>
   <main class="bitrix-page">
 
-    <BitrixHero />
+    <WidgetsBitrix24HeroUiBitrixHero/>
 
-    <BitrixAdvantages />
+    <WidgetsBitrix24AdvantagesUiBitrixAdvantages/>
 
-    <BitrixServices />
+    <WidgetsBitrix24ServicesUiBitrixServices/>
 
-    <BitrixProcess />
+    <WidgetsBitrix24ProcessUiBitrixProcess />
 
-    <BitrixCTA />
+    <WidgetsBitrix24CTAUiBitrixCTA />
 
   </main>
 </template>
